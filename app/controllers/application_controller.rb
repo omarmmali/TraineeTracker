94130f65360a8cc741d9330eb8b8b4421034bca7
Omar M. Ali <omarmaboelenen@gmail.com>
2017-09-27 10:07:19 +0200

    added queries and trainee submissions total

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def index
-  	@tracked_trainees = Trainee.all.where(tracking_status: 1)
+    @tracked_trainees = Trainee.all.where(tracking_status: 1)
+    @submissions = Submission.all
   end
 end
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,13 @@
 class SubmissionsController < ApplicationController
   def index
-    @sumbissions = Sumbission.all.where(trainee_id: params[:trainee_id])
+    @trainee = Trainee.find(params[:trainee_id])
+    @submissions = Submission.all.where(trainee_id: params[:trainee_id])
+    if params[:date].present?
+      @submissions = @submissions.where(
+        'DATE(created_at) BETWEEN ? AND ?',
+        params[:date][:from_date],
+        params[:date][:to_date]
+      )
+    end
   end
 end
